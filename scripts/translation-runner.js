9fe19ad3d34db1b10793ec1a5e990c0545f4b391
Erko Bridee <erko.bridee@gmail.com>
2019-07-01 08:42:29 +0200

    update(scripts): translation-runner

--- scripts/translation-runner.js
+++ scripts/translation-runner.js
@@ -1,10 +1,39 @@
-const manageTranslations = require('react-intl-translations-manager').default;
+const { readFileSync } = require('fs');
+const {
+	default: manageTranslations,
+	readMessageFiles,
+	getDefaultMessages,
+} = require('react-intl-translations-manager');
+
+const messagesDirectory = '.i18nExtractedMessages';
+const defaultLanguage = 'en';
+
+const readFile = filename => {
+	try {
+		return readFileSync(filename);
+	} catch (error) {
+		return undefined;
+	}
+};
 
 manageTranslations({
-	messagesDirectory: '.i18nExtractedMessages',
+	messagesDirectory: messagesDirectory,
 	translationsDirectory: 'src/assets/languages/',
 	whitelistsDirectory: 'src/assets/languages/whitelists/',
-	languages: ['en', 'es', 'pt'],
+	languages: [defaultLanguage, 'es', 'pt'],
+	overrideCoreMethods: {
+		// based on:
+		// https://github.com/GertjanReynaert/react-intl-translations-manager/issues/76#issuecomment-283186721
+		provideWhitelistFile: langResults => {
+			if (langResults.lang === defaultLanguage) {
+				const messageFiles = readMessageFiles(messagesDirectory);
+				const messages = getDefaultMessages(messageFiles).messages;
+				return Object.keys(messages);
+			}
+			const jsonFile = readFile(langResults.whitelistFilepath);
+			return jsonFile ? JSON.parse(jsonFile) : undefined;
+		},
+	},
 });
 
 console.log('\ntranslation runner done\n\n');
