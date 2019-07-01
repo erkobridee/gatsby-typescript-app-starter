2c0d137336e777902e9c8646e05ee2e718c6d5e1
Erko Bridee <erko.bridee@gmail.com>
2019-07-01 08:15:01 +0200

    update(languages): move whitelist messages to its own directory

--- scripts/translation-runner.js
+++ scripts/translation-runner.js
@@ -3,6 +3,7 @@ const manageTranslations = require('react-intl-translations-manager').default;
 manageTranslations({
 	messagesDirectory: '.i18nExtractedMessages',
 	translationsDirectory: 'src/assets/languages/',
+	whitelistsDirectory: 'src/assets/languages/whitelists/',
 	languages: ['en', 'es', 'pt'],
 });
 
