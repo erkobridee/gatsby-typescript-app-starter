80083773b0f753e310f934b456044ad507731040
Erko Bridee <erko.bridee@gmail.com>
2019-07-29 16:51:27 +0200

    update(pages): 404 initialization flow

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,21 +4,31 @@ import { FormattedMessage } from 'react-intl';
 import Layout from 'components/Layout';
 import SEO from 'components/SEO';
 
-const NotFoundPage = () => (
-	<Layout>
-		<FormattedMessage id="404.title" defaultMessage="404: Not found">
-			{text => <SEO title={text as string} />}
-		</FormattedMessage>
-		<h1>
-			<FormattedMessage id="404.title" defaultMessage="404: Not found" />
-		</h1>
-		<p>
-			<FormattedMessage
-				id="404.message"
-				defaultMessage="You just hit a route that doesn&#39;t exist... the sadness."
-			/>
-		</p>
-	</Layout>
-);
+const NotFoundPage = () => {
+	const [isInitialized, setIsInitialized] = React.useState(false);
+	React.useEffect(() => {
+		setIsInitialized(true);
+	});
+	return (
+		<Layout>
+			{isInitialized && (
+				<>
+					<FormattedMessage id="404.title" defaultMessage="404: Not found">
+						{text => <SEO title={text as string} />}
+					</FormattedMessage>
+					<h1>
+						<FormattedMessage id="404.title" defaultMessage="404: Not found" />
+					</h1>
+					<p>
+						<FormattedMessage
+							id="404.message"
+							defaultMessage="You just hit a route that doesn&#39;t exist... the sadness."
+						/>
+					</p>
+				</>
+			)}
+		</Layout>
+	);
+};
 
 export default NotFoundPage;
