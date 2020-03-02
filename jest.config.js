860b5f1b473ecc92bf976c9589b7763160e92437
Erko Bridee <erko.bridee@gmail.com>
2020-03-02 10:07:19 +0100

    feat(jest): add svgr support

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,8 @@ module.exports = {
 	preset: 'ts-jest',
 	moduleNameMapper: {
 		'.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
-		'.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.js`,
+		'.+\\.svg$': `<rootDir>/__mocks__/svgr-mock.js`,
+		'.+\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.js`,
 	},
 	moduleDirectories: ['node_modules', 'src'],
 	testPathIgnorePatterns: ['node_modules', '.cache', 'public'],
